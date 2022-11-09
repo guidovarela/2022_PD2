3cf4fbccbc95f0794ed21c6a241ee4c7ac388678
Guido Varela Coletta <guido.varela@gmail.com>
2022-11-09 16:57:50 -0300

    clase 9-11

--- 08_Javascript/pruebas/js/condicionales.js
+++ 08_Javascript/pruebas/js/condicionales.js
@@ -23,3 +23,33 @@ if(alta_usuario == true){
 }else{
     alert('No estas registrado :(')
 }
+
+//Condicional sin else
+if(nombre_usuario != null){
+    alert('Hola '+nombre_usuario)
+}else if(nombre_usuario == null){
+    alert('Debe completar su nombre')
+}
+
+let nota = parseInt(prompt('Insertar calificacion: '))
+
+/* 0-3 desaprobado */
+/* 4-6 regular */
+/* 7-8 Muy bueno */
+/* 9-10 excelente */
+
+/* 
+|| or
+&& and
+*/
+
+if(nota <= 3){
+    console.log('desaprobado')
+}else if(nota > 4 && nota <= 6){
+    console.log('regular')
+}else if(nota > 6 && nota <=8){
+    console.log('Muy bueno')
+}else{
+    console.log('Excelente')
+}
+
---
+++ 09_JS/funciones.js
@@ -0,0 +1,25 @@
+/*
+        function nombre(parametros1,parametros2) {
+            //instrucciones de la funcion
+            //Scope
+            //las variables que se definen en la funcion, son de tipo local (no fucionan fuera de la funcion)
+            //Las funciones pueden utilizar variables globales (externas a la funcion)
+        }
+*/
+//1er se debe definir la funcion        
+//2do puede ejecutarse
+
+//funcion sin parametros
+//mensaje()
+
+//funcion con parametro
+/* mensaje2('Pepe')
+mensaje2('Maria')
+mensaje2("Sofia") */
+
+
+//No debe coincidir el nombre de la funcion, con alguna variable local de la misma
+/* function sumar(a,b){
+    suma=a+b
+    console.log(suma)
+} */
